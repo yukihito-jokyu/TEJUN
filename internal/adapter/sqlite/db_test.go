269ff9e1cd7faea73dfb2bc4dcbaab2ce08e9269
jokyu yukihito <ok220020@oks.iput.ac.jp>
2026-09-26 14:30:46 +0900

    ✨ エージェント接続をSQLiteへ保存 #4

--- internal/adapter/sqlite/db_test.go
+++ internal/adapter/sqlite/db_test.go
@@ -46,8 +46,8 @@ func TestOpenAppliesMigrationsAndPragmas(t *testing.T) {
 				t.Fatal(err)
 			}
 
-			if version != 1 {
-				t.Fatalf("version=%d, want 1", version)
+			if version != 2 {
+				t.Fatalf("version=%d, want 2", version)
 			}
 
 			assertPragmasOnTwoConnections(t, db)
